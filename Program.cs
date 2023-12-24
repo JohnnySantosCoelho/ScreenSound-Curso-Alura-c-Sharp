3c08e65f58e2d09c9e62d652e155cfa1d1e2a9f2
johnnysantoscoelho <johnny.santosgomes@gmail.com>
2023-12-24 13:48:00 -0300

    Segundo Curso de C Sharp

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Fauline");
+ep1.AdicionarConvidados("Johnny");
+
+
+Episodio ep2 = new(2, "Técnicas de aprendizado", 50);
+ep2.AdicionarConvidados("Yasmim");
+ep2.AdicionarConvidados("Diego");
+ep2.AdicionarConvidados("Veronica");
+
+
+Podcast podcast = new("Podcast especial", "Daneil");
+podcast.AdicionaEpisodio(ep1);
+podcast.AdicionaEpisodio(ep2);
+podcast.ExibirDetalhes();
